3c048f5973eb1ceedc6859d7b7f8d4d0e3d5ca9f
Bobby Coleman <colemankidding@gmail.com>
2020-06-04 14:28:44 -0700

    Add proxy to package.json to allow AJAX calls to port 3001

--- server.js
+++ server.js
@@ -15,4 +15,10 @@ app.use(express.static(path.join(__dirname, 'build')));
 
 app.get('/*', (req, res) => {
     res.sendFile(path.join(__dirname, 'build', 'index.html'));
-  });
+});
+
+const port = process.env.PORT || 3001;
+
+app.listen(port, () => {
+  console.log(`Express app running on port ${port}`)
+});
