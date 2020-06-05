efeca636eec29f48783cca296f973ea8deb06491
Bobby Coleman <colemankidding@gmail.com>
2020-06-04 22:57:31 -0700

    Add route which renders notebook page

--- src/App.js
+++ src/App.js
@@ -3,13 +3,14 @@ import { BrowserRouter as Router, Route } from "react-router-dom"
 import './App.css';
 
 import Navbar from "./components/navbar";
-
+import Notebook from "./components/notebook/notebook";
 
 const App = () => {
   return (
     <Router>
       <div className="App">
         <Navbar />
+        <Route path="/notebook" exact component={Notebook} />
       </div>
     </Router>
   );
