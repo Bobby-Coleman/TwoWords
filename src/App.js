580b5ccd73ae856a0a8325a3119cd92e676c1d49
Bobby Coleman <colemankidding@gmail.com>
2020-06-05 20:38:09 -0700

    Add simple footer

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React from 'react';
 import { BrowserRouter as Router, Route } from "react-router-dom"
 import './App.css';
 
+import Home from "./components/home/home";
 import Navbar from "./components/navbar/navbar";
 import Notebook from "./components/notebook/notebook";
 import About from "./components/about/about";
 import Login from "./components/login/login";
 import Signup from "./components/signup/signup";
+
 
 const App = () => {
   return (
@@ -15,12 +17,18 @@ const App = () => {
         <div className="App-navbar">
           <Navbar />
         </div>
+        <Route path="/" exact component={Home} />
         <Route path="/notebook" exact component={Notebook} />
         <Route path="/about" exact component={About} />
         <Route path="/login" exact component={Login} />
         <Route path="/signup" exact component={Signup} />
-        App
       </div>
+      <footer>
+          Copyright Ⓒ Bobby Coleman, 2020 
+          <div className="footer-right">
+          Until the robots take over, enjoy!
+          </div>
+      </footer>
     </Router>
   );
 }
