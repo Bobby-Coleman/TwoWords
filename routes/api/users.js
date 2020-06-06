4f04db53e9c12bc914d9de1e796ba44c470ba8d7
Bobby Coleman <colemankidding@gmail.com>
2020-06-06 10:44:12 -0700

    Require user api route in server, fix config database

---
+++ routes/api/users.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const router = express.Router();
+
+const User = require('../../models/User')
+
+
+router.post('/', (req, res) => {
+    res.send('registered');
+})
+
+module.exports = router;
--- server.js
+++ server.js
@@ -16,6 +16,7 @@ app.use(favicon(path.join(__dirname, 'build', 'favicon.ico')));
 app.use(express.static(path.join(__dirname, 'build')));
 
 //mount API routes here
+app.use('/api/users', require('./routes/api/users'))
 
 app.get('/*', (req, res) => {
     res.sendFile(path.join(__dirname, 'build', 'index.html'));
