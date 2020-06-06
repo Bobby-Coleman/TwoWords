3904cd8de8d85191a9ad13821d7beb600a3050a4
Bobby Coleman <colemankidding@gmail.com>
2020-06-06 12:20:45 -0700

    Impliment post request to /api/users/signup and add user

--- controllers/users.js
+++ controllers/users.js
@@ -1,5 +1,5 @@
 const User = require('../models/User')
-
+const bcrypt = require('bcryptjs');
 
 const signup = (req, res) => {
     const { name, email, password } = req.body;
@@ -7,7 +7,7 @@ const signup = (req, res) => {
     if(!name || !email || !password) {
         return res.status(400).json({ msg: 'Please enter all fields'})
     }
-
+    // Check for existing user email
     User.findOne( {email} )
         .then(user => {
           if(user) return res.status(400).json({ msg: 'A user with that email already exists'});
@@ -17,6 +17,23 @@ const signup = (req, res) => {
             email,
             password
           });
+          // Create salt and hash password
+          bcrypt.genSalt(10, (err, salt) => {
+              bcrypt.hash(newUser.password, salt, (err, hash) => {
+                  if(err) throw err;
+                  newUser.password = hash;
+                  newUser.save()
+                    .then(user => {
+                        res.json({
+                          user: {
+                            id: user.id,
+                            name: user.name,
+                            email: user.email
+                          }
+                      })
+                  })
+              })
+         })
     })
 };
 
