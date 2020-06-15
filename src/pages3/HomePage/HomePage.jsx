6af61d82027521aeb702a2876c4e4ec5e3ac2250
Bobby Coleman <colemankidding@gmail.com>
2020-06-15 16:49:58 -0700

    Start in on async api fetch of two words

--- src/pages3/HomePage/HomePage.jsx
+++ src/pages3/HomePage/HomePage.jsx
@@ -1,12 +1,18 @@
 import React, { Component } from 'react';
 
-export default class Navbar extends Component {
+export default class Home extends Component {
+
+  state = {
+    loading: true
+  }
+
+  async componentDidMount() {
+    const url = "https"
+  }
 
   render() {
-    return (
-      <div className="notebook">
-        <h1>Home</h1>
+    return <div className="home">
+        {this.state.loading ? <div>Two Words</div> : <div>Words</div>} 
       </div>
-    );
   }
 }
