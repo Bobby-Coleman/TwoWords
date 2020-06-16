df7d787405b397f46fea6ec7f42a2852da9a39c7
Bobby Coleman <colemankidding@gmail.com>
2020-06-16 15:39:38 -0700

    Fetch two random words and display them

--- src/pages3/TwoWordsPage/TwoWordsPage.jsx
+++ src/pages3/TwoWordsPage/TwoWordsPage.jsx
@@ -4,26 +4,34 @@ const API_KEY = process.env.API_KEY
 export default class TwoWords extends Component {
 
   state = {
-    loading: true,
+    loadingFirstWord: true,
+    loadingSecWord: true,
     wordOne: null,
+    wordTwo: null,
   };
 
 
   async componentDidMount() {
-    const firstWordUrl = `https://api.wordnik.com/v4/words.json/randomWord?hasDictionaryDef=true&includePartOfSpeech=noun&minCorpusCount=70000&maxCorpusCount=-1&minDictionaryCount=1&maxDictionaryCount=-1&minLength=3&maxLength=7&api_key=es8h82u4zompkf5qscrsarq0nfwr7qkscofpl8t20ohibukz7`
-    const response = await fetch(firstWordUrl);
-    const data = await response.json();
-    this.setState({wordOne: data.word, loading: false})
+    const firstWordUrl = `https://api.wordnik.com/v4/words.json/randomWord?hasDictionaryDef=true&includePartOfSpeech=verb%2Cnoun&minCorpusCount=70000&maxCorpusCount=-1&minDictionaryCount=1&maxDictionaryCount=-1&minLength=5&maxLength=-1&api_key=es8h82u4zompkf5qscrsarq0nfwr7qkscofpl8t20ohibukz7`
+    const secondWordUrl = `https://api.wordnik.com/v4/words.json/randomWord?hasDictionaryDef=true&includePartOfSpeech=noun&minCorpusCount=70000&maxCorpusCount=-1&minDictionaryCount=1&maxDictionaryCount=-1&minLength=3&maxLength=7&api_key=es8h82u4zompkf5qscrsarq0nfwr7qkscofpl8t20ohibukz7`
+
+    const firstWordRes = await fetch(firstWordUrl);
+    const secWordRes = await fetch(secondWordUrl);
+    const firstData = await firstWordRes.json();
+    const secondData = await secWordRes.json();
+    this.setState({wordOne: firstData.word, loadingFirstWord: false})
+    this.setState({wordTwo: secondData.word, loadingSecWord: false})
   }
 
   render() {
     return (
     <div className="two-words-container">
-       {this.state.loading || !this.state.wordOne ? (
-         <div>loading...</div>
+       {this.state.loadingFirstWord || this.state.loadingSecWord || !this.state.wordOne || !this.state.wordTwo ? (
+         <div>Loading Two Words</div>
        ) : (
         <div>
           <div>{this.state.wordOne}</div>
+          <div>{this.state.wordTwo}</div>
         </div>
        )}
     </div>
