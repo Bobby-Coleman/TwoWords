2c83cc2a5104cdca2a9d9d9fa90a78bfbcc7d14b
Bobby Coleman <colemankidding@gmail.com>
2020-06-16 15:15:37 -0700

    Fetch word one and complete conditional which displays whether its loading or fetched

--- src/pages3/TwoWordsPage/TwoWordsPage.jsx
+++ src/pages3/TwoWordsPage/TwoWordsPage.jsx
@@ -13,12 +13,20 @@ export default class TwoWords extends Component {
     const firstWordUrl = `https://api.wordnik.com/v4/words.json/randomWord?hasDictionaryDef=true&includePartOfSpeech=noun&minCorpusCount=70000&maxCorpusCount=-1&minDictionaryCount=1&maxDictionaryCount=-1&minLength=3&maxLength=7&api_key=es8h82u4zompkf5qscrsarq0nfwr7qkscofpl8t20ohibukz7`
     const response = await fetch(firstWordUrl);
     const data = await response.json();
-    this.setState({wordOne: data.word})
+    this.setState({wordOne: data.word, loading: false})
   }
 
   render() {
-    return <div className="two-words-container">
-       {this.state.loading || !this.state.wordOne ? <div>loading...</div> : <div>word...</div>}
-      </div>
+    return (
+    <div className="two-words-container">
+       {this.state.loading || !this.state.wordOne ? (
+         <div>loading...</div>
+       ) : (
+        <div>
+          <div>{this.state.wordOne}</div>
+        </div>
+       )}
+    </div>
+    );
   }
 }
