1bb96c725f5b5271c4da29a7ad9d195ba7ff9db1
Bobby Coleman <colemankidding@gmail.com>
2020-06-16 16:54:04 -0700

    Refactor to use random words npm package

--- src/pages3/TwoWordsPage/TwoWordsPage.jsx
+++ src/pages3/TwoWordsPage/TwoWordsPage.jsx
@@ -1,44 +1,27 @@
 import React, { Component } from 'react';
 import './TwoWordsPage.css'
-const API_KEY = process.env.API_KEY
+const randomWords = require('random-words');
 
 export default class TwoWords extends Component {
 
   state = {
-    loadingFirstWord: true,
-    loadingSecWord: true,
     wordOne: null,
     wordTwo: null,
   };
 
-
-  async componentDidMount() {
-    const firstWordUrl = `https://api.wordnik.com/v4/words.json/randomWord?hasDictionaryDef=true&includePartOfSpeech=verb%2Cnoun&minCorpusCount=70000&maxCorpusCount=-1&minDictionaryCount=1&maxDictionaryCount=-1&minLength=5&maxLength=-1&api_key=es8h82u4zompkf5qscrsarq0nfwr7qkscofpl8t20ohibukz7`
-    const secondWordUrl = `https://api.wordnik.com/v4/words.json/randomWord?hasDictionaryDef=true&includePartOfSpeech=noun&minCorpusCount=70000&maxCorpusCount=-1&minDictionaryCount=1&maxDictionaryCount=-1&minLength=3&maxLength=7&api_key=es8h82u4zompkf5qscrsarq0nfwr7qkscofpl8t20ohibukz7`
-
-    const firstWordRes = await fetch(firstWordUrl);
-    const secWordRes = await fetch(secondWordUrl);
-    const firstData = await firstWordRes.json();
-    const secondData = await secWordRes.json();
-    const firstWord = firstData.word.toUpperCase();
-    const secondWord = secondData.word.toUpperCase();
-    this.setState({wordOne: firstWord, loadingFirstWord: false})
-    this.setState({wordTwo: secondWord, loadingSecWord: false})
-}
+  getWords = () => {
+    const words = randomWords({exactly: 2, maxLength: 7, formatter: (word)=> word.toUpperCase()})
+    const wordOne = words[0]
+    const wordTwo = words[1]
+    this.setState({wordOne: wordOne})
+    this.setState({wordTwo: wordTwo})
+  }
+  
+  componentDidMount() {
+    this.getWords()
+  }
 
   render() {
-
-    if (this.state.loadingFirstWord || this.state.loadingSecWord) {
-      return <div>Loading Two Words</div>
-    }
-
-    if (!this.state.wordOne) {
-      return <div>Couldn't retreive first word :(</div>
-    }
-
-    if (!this.state.wordTwo) {
-      return <div>Couldn't retreive second word :(</div>
-    }
 
     return (
     <div className="two-words-container">
